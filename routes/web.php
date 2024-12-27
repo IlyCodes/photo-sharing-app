b5253d2efcadc10918be75de7be75d62158a97b9
IlyCodes <ilycode13@gmail.com>
2024-12-27 23:49:34 +0100

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,12 +15,17 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::resource('photos', PhotoController::class);
+    Route::get('/my-gallery', [PhotoController::class, 'myGallery'])->name('photos.my-gallery');
+
+    Route::post('/new-comment/{photo_id}', [CommentController:: class, 'store'])->name('comments.store');
+    Route::delete('/comment/{id}', [CommentController:: class, 'destroy'])->name('comments.destroy');
+
+    Route::post('/vote', [VoteController::class, 'store'])->name('votes.store');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::resource('photos', PhotoController::class);
-    Route::get('/my-gallery', [PhotoController::class, 'myGallery'])->name('photos.my-gallery');
 });
 
 require __DIR__.'/auth.php';
