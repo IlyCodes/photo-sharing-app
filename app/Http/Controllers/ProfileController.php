3416b8ae6bd79c647b78530d5fcceb59441797bb
IlyCodes <ilycode13@gmail.com>
2025-01-08 11:53:02 +0100

    Add new method to profile controller to drop user image file

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 
@@ -32,10 +34,14 @@ class ProfileController extends Controller
             $request->user()->email_verified_at = null;
         }
 
-        if ($request->user()->image) {
+        $user = User::find(Auth::user()->id);
+        $user_image = $user->image;
+
+        if ($request->user()->image != $user_image) {
             $imageName = uniqid() . '.' . $request->user()->image->extension();
             $request->user()->image->move(public_path('user_img'), $imageName);
             $request->user()->image = 'user_img/' . $imageName;
+            $this->dropImage($user_image);
         }
 
         $request->user()->save();
@@ -57,10 +63,21 @@ class ProfileController extends Controller
         Auth::logout();
 
         $user->delete();
+        $this->dropImage($user->image);
 
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
         return Redirect::to('/');
+    }
+
+    /**
+     * Delete the user's image file from [user_img] folder.
+     */
+    public function dropImage($user_image)
+    {
+        if (File::exists($user_image)) {
+            File::delete($user_image);
+        }
     }
 }
